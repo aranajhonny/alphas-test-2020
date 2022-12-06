40ace5fca7c9fc9128492708e3e802eaacbb8e24
Jhonny <aranajhonny@users.noreply.github.com>
2022-12-06 10:34:55 -0500

    primer program

---
+++ acme/index.ts
@@ -0,0 +1,8 @@
+// `nodes` contain any nodes you add from the graph (dependencies)
+// `root` is a reference to this program's root node
+// `state` is an object that persist across program updates. Store data here.
+import { nodes, root, state } from 'membrane';
+
+export function setup() {
+  console.log("Hello World");
+}
