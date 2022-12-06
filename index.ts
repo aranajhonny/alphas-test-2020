1e653f02c8344465292e259f4448e6db5313ac6d
Jhonny <aranajhonny@users.noreply.github.com>
2022-12-05 21:55:55 -0500

    Squashed 'email/' content from commit 79b0b47
    git-subtree-dir: email
    git-subtree-split: 79b0b47b96bbcf024e8ddd43dbf3c05150740673

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { root, nodes, state } from "membrane";
+const { sys_user } = nodes;
+
+export const Root = {
+  configure: async ({ args }) =>
+    await sys_user.configureEmail({ ...args }).$invoke(),
+
+  handleEmail: async ({ args: { message } }) => {
+    await root.received.$emit({ message });
+  },
+
+  send: async ({ self, args }) => {
+    await sys_user.email.tell({ ...args }).$invoke();
+  },
+};
+
